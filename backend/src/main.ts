1a5b18587aa55bbcdac88f056c6e754ae6a7b32d
lvodak <lvodak@student.42belgium.be>
2026-05-25 22:56:28 +0200

    Https set up with nginx; auth & users service files created (not defined)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors();
+  app.getHttpAdapter().getInstance().set('trust proxy', 1);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

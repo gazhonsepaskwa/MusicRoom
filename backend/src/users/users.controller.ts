2c84c886246f83a3e940df92b9349551172c14c1
lvodak <lvodak@student.42belgium.be>
2026-05-29 11:07:34 +0200

    Prisma correctly included as a nestjs service; Corrected include path and package.json

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+	constructor(private readonly usersService: UsersService) {}
+
+	@Get(':id')
+	getUser(@Param('id') id: number,) {
+		return this.usersService.user({
+			id: id,
+		});
+	}
+
+	@Post()
+	createUser(@Body() body: {
+			password: string;
+			name: string;
+			email: string;},)
+	{
+		this.usersService.createUser({
+			password: body.password,
+			username: body.name,
+			email: body.email,
+		});
+	}
 }

36ceeae4b84de1a95600319ce9ce3165a54c3bd2
Nathan Lebrun <gaz.honsepaskwa@gmail.com>
2026-06-05 10:33:37 +0200

    Search result

--- android-app/app/src/main/java/be/nalebrun/musicroom/MainActivity.kt
+++ android-app/app/src/main/java/be/nalebrun/musicroom/MainActivity.kt
@@ -1,17 +1,24 @@
 package be.nalebrun.musicroom
 
+import android.app.appsearch.SearchResult
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import be.nalebrun.musicroom.ui.theme.MusicRoomTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,20 +26,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ArtistCardUi()
+            MusicRoomTheme() {
+                SearchResultUi()
+            }
         }
     }
 }
 
 @Composable
-fun ArtistCardUi() {
-    Column {
-        ArtistCard()
+fun SearchResultUi() {
+    Column(
+        modifier = Modifier
+            .padding(top = 30.dp)
+            .background(Color.White)
+    ) {
+        SearchResultCard(ResultType.MUSIC, "Scared of the dark", "Em Beihold ● Tales of a failed shapeshifter")
+        HorizontalDivider(thickness = 1.dp, color = Color.Gray)
+        SearchResultCard(ResultType.PLAYLIST, "Bestof '26", "nalebrun")
+        HorizontalDivider(thickness = 1.dp, color = Color.Gray)
+        SearchResultCard(ResultType.ALBUM, "Tales of a failed shapeshifter", "Em Beihold ● 11 song")
+        HorizontalDivider(thickness = 1.dp, color = Color.Gray)
+        SearchResultCard(ResultType.ARTIST, "Em Beihold", "5 albums ● 34 songs")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ArtistCardUi()
+    SearchResultUi()
 }
--- android-app/app/src/main/java/be/nalebrun/musicroom/layouts.kt
+++ android-app/app/src/main/java/be/nalebrun/musicroom/layouts.kt
@@ -1,43 +1,78 @@
 package be.nalebrun.musicroom
 
+import android.app.appsearch.SearchResult
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import coil3.compose.AsyncImage
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil3.Image
 
+enum class ResultType {
+    ARTIST,
+    MUSIC,
+    PLAYLIST,
+    ALBUM
+}
+
+/**
+ * Component to display a search result
+ * @param resultType Type of the result
+ * @param title First line to display
+ * @param subtitle Second line to display
+ */
 @Composable
-fun ArtistCard() {
+fun SearchResultCard(resultType: ResultType, title: String, subtitle : String) {
     Row (
         horizontalArrangement = Arrangement
             .spacedBy(10.dp)
         ,
         modifier = Modifier
-            .padding(top = 30.dp)
+            .padding(top = 2.dp, bottom = 2.dp, start = 10.dp)
             .background(Color.White)
-            .height(40.dp)
+            .height(50.dp)
             .fillMaxWidth()
+        ,
+        verticalAlignment = Alignment.CenterVertically
     ){
-        AsyncImage(
-            model = "https://i.scdn.co/image/ab67616d0000b273dd3a17393ca3f47e4c523c26",
-            contentDescription = null,
+//        AsyncImage(
+//            modifier = Modifier.padding(5.dp),
+//            model = "https://i.scdn.co/image/ab67616d0000b273dd3a17393ca3f47e4c523c26",
+//            contentDescription = null,
+//        )
+        Image(
+            modifier = Modifier
+                .padding(2.dp),
+            painter = painterResource(id = when(resultType) {
+                ResultType.ARTIST -> R.drawable.artist
+                ResultType.MUSIC -> R.drawable.note_1
+                ResultType.PLAYLIST -> R.drawable.playlist_tmp
+                ResultType.ALBUM -> R.drawable.album
+            }),
+            contentDescription = ""
         )
         Column (
         ) {
-            Text("Renaud")
-            Text("x albums")
+            Text(title, fontWeight = FontWeight.Bold)
+            Text(subtitle)
         }
     }
 }

b842fa140d77c7da46d8ca95747678b5eca5b7fa
gaz honsepaskwa <101428905+gazhonsepaskwa@users.noreply.github.com>
2026-06-10 23:08:06 +0200

    Merge spoti scraper to dev (#6)
    * main.py script to get data from spotify via the api. created api folder for api related functions. and other test files that will be removed. next : add to the database the scraped metadata
    
    * commit premerge from origin/backend/userAuth to get db viewer
    
    * became a backend api. routes: 2/3 0.0.0.0:1919/add_album 0.0.0.0:1919/add_track done. missing : add_artist. also missing link entry between artist and music in the db
    
    * added route : add_artist. also added link entry between artist and music in the db and between music and artist. cleaned up a little the code. more cleanup to be done and little improvements
    
    * added route : add_artist. also added link entry between artist and music in the db and between music and artist. cleaned up a little the code. more cleanup to be done and little improvements (forgotten files)
    
    * added Exeption handeling and error requests. added utils file
    
    * added youtube download for track -#
    
    * krob_scraper
    
    * change structure of app.py for clarity
    
    * removed old folder
    
    ---------
    
    Co-authored-by: Nathan Lebrun <gaz.honsepaskwa@gmail.com>
    Co-authored-by: Xekhtyrl <lvfa1993@hotmail.com>

--- spoti-scraper/api/utils.py
+++ spoti-scraper/api/utils.py
@@ -2,10 +2,21 @@
 Utility functions for the Spotify and Youtube API.
 """
 
+import requests
+
+
+class RateLimitError(Exception):
+    """Raised when the rate limit is exceeded"""
+
+    pass
+
+
 def check_response(response) -> None:
     """Raise an exception if the response status code is not 200"""
     if response.status_code != 200:
         if response.status_code == 429:
-            raise requests.exceptions.RequestException(f"Rate limited. Retry after {response.headers.get("Retry-After", 1)} seconds.")
+            raise RateLimitError(f"{response.headers.get('Retry-After', 1)}")
         else:
-            raise requests.exceptions.RequestException(f"Api error : {response.status_code} - {response.text}")
+            raise requests.exceptions.RequestException(
+                f"Api error : {response.status_code} - {response.text}"
+            )

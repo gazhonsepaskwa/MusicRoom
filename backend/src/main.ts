1e1383a0d5e61957539f744ca6fd469d0002c545
Work <work@Works-MacBook-Pro.local>
2026-06-12 20:45:12 +0200

    replaced nginx by caddy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.getHttpAdapter().getInstance().set('trust proxy', 1);

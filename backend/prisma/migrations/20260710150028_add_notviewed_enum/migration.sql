ed3e7673c75f3403afe5f781ecd2bb3fa33778b2
lvodak <lvodak@student.42.be>
2026-07-13 23:45:24 +0200

    removed migrations from git

--- backend/prisma/migrations/20260710150028_add_notviewed_enum/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterEnum
-ALTER TYPE "invitationStatus" ADD VALUE 'NOTVIEWED';
-
--- AlterTable
-ALTER TABLE "friendship" ALTER COLUMN "status" SET DEFAULT 'PENDING';
--- backend/prisma/migrations/20260710201556_user_preference_update/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "user" ADD COLUMN     "showPreferedMusics" "visibilityStatus" NOT NULL DEFAULT 'PUBLIC';

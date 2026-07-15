9dd33eb5837abfb238515f9cb3dcfbe7bd00174c
lvodak <lvodak@student.42.be>
2026-07-15 18:04:45 +0200

    pre merged with dev

--- backend/src/friendship/dto/friendship-response.dto.ts
+++ backend/src/friendship/dto/friendship-response.dto.ts
@@ -7,21 +7,6 @@ export class FriendshipResponseDto {
   message!: string;
 }
 
-export class FriendshipItemDto {
-	@ApiProperty({example: 1})
-	requesterId!: number
-	@ApiProperty({example: 2})
-	addresseeId!: number
-	@ApiProperty({
-		enum: invitationStatus,
-		description: 'The status of the friendship request',
-		example: invitationStatus.ACCEPTED,
-	})
-	status!: invitationStatus
-	@ApiProperty({example: "2026-07-10T22:32:27.254Z"})
-	createdAt!: Date
-}
-
 export class FriendshipDto {
 	@ApiProperty({
 		enum: invitationStatus,

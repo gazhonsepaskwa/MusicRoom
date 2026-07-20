eaef3187847a6fd412a5dbd087e081a06915a80c
Leopoldo Vodak <148996604+Xekhtyrl@users.noreply.github.com>
2026-07-20 17:47:30 +0200

    Merge user profile to dev (#51)
    * main.py script to get data from spotify via the api. created api folder for api related functions. and other test files that will be removed. next : add to the database the scraped metadata
    
    * commit premerge from origin/backend/userAuth to get db viewer
    
    * became a backend api. routes: 2/3 0.0.0.0:1919/add_album 0.0.0.0:1919/add_track done. missing : add_artist. also missing link entry between artist and music in the db
    
    * Prisma correctly included as a nestjs service; Corrected include path and package.json
    
    * first push
    
    * users and authentification implemented with JWT; Implemented JWT Protection by default, need @Public for public API
    
    * Implement docker secrets and adapt all files to it
    
    * Implement docker secrets and adapt all files to it; Add the mail confirmation to the user creation with a Mail Service;
    
    * OAuth implementation in progress (waiting for tests); added script to update db with secrets and written how to do so in readme
    
    * OAuth implemented. Need further test when frontend connected
    
    * Applied DTO for auth service
    
    * updated readme for backend structure changes
    
    * prisma update: add field to existing tables and created profile table
    
    * add API to playlists to add/remove songs, and get favorite
    
    * apply swaggers to playlist + create favorite
    
    * protect playlist deletion; add type to artist for search purpose; add new route to artist to get the artist musics
    
    * change controllers param to dto
    
    * redirect login
    
    * verifyEmail for dev
    
    * migrations update
    
    * email set in lowercase in db; created api to get profile based on visibility
    
    * API update profile
    
    * friendship api list corrected
    
    * account deletion; Profile, get and update, improved and fixed
    
    * removed migrations from git
    
    * account deletion response changed
    
    * account deletion response changed
    
    * secured unwanted auth without verification. Link for redirection commented to be used later. Profile page API done
    
    * pre merged with dev
    
    * pre merged with dev
    
    * api for owner profile added
    
    ---------
    
    Co-authored-by: Nathan Lebrun <gaz.honsepaskwa@gmail.com>
    Co-authored-by: lvodak <lvodak@student.42belgium.be>
    Co-authored-by: Aurèle Josserand <aurele.josserand@gmail.com>
    Co-authored-by: lvodak <lvodak@student.42.be>

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,48 +1,51 @@
 import {
   Controller,
   Get,
-  Post,
   Param,
   Body,
   Query,
   BadRequestException,
   Patch,
-  UnauthorizedException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiOkResponse, ApiQuery } from '@nestjs/swagger';
 import { UserProfileResponseDto, UserResponseDto, UserUpdateDto } from './dto/user.dto';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { ParseSafeIntPipe } from '../common/pipe/parse_safe_int.pipe';
-import { profile } from 'console';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  //a garder ou pas ?
-  @ApiQuery({ name: 'id', required: false, type: String })
-  @ApiQuery({ name: 'username', required: false, type: String })
-  @ApiOkResponse({ type: UserResponseDto })
-  @Get()
-  async getUser(
-    @Query('id') id?: string,
-    @Query('username') username?: string,
-  ) {
-    let user: any = null;
-    if (id) {
-      user = await this.usersService.user({ id: +id });
-    }
+//   //a garder ou pas ?
+//   @ApiQuery({ name: 'id', required: false, type: String })
+//   @ApiQuery({ name: 'username', required: false, type: String })
+//   @ApiOkResponse({ type: UserResponseDto })
+//   @Get()
+//   async getUser(
+//     @Query('id') id?: string,
+//     @Query('username') username?: string,
+//   ) {
+//     let user: any = null;
+//     if (id) {
+//       user = await this.usersService.user({ id: +id });
+//     }
 
-    if (username) {
-      user = await this.usersService.user({ username });
-    }
-    if (user == null) throw new BadRequestException();
-    return {
-      id: user.id,
-      username: user.username,
-      email: user.email,
-    };
+//     if (username) {
+//       user = await this.usersService.user({ username });
+//     }
+//     if (user == null) throw new BadRequestException();
+//     return {
+//       id: user.id,
+//       username: user.username,
+//       email: user.email,
+//     };
+//   }
+
+  @ApiOkResponse({type: UserProfileResponseDto})
+  @Get('profile')
+  async getOwnProfile(@CurrentUser() userId: number) {
+	return await this.usersService.getUserProfile(userId, userId);
   }
 
   @ApiOkResponse({type: UserProfileResponseDto})

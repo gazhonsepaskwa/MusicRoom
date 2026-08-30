f78efdee1f9f883913d3c01d81ad4277a836c288
gaz honsepaskwa <web@nalebrun.be>
2026-08-30 15:22:12 +0200

    made the album image bigger and removed the one on the profile and replaced it with a button that show it in full screen

--- android-app/app/src/main/java/be/nalebrun/musicroom/ui/screen/AlbumUi.kt
+++ android-app/app/src/main/java/be/nalebrun/musicroom/ui/screen/AlbumUi.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -29,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -78,15 +78,46 @@ fun AlbumUi(albumId: Int) {
             Row (
                 horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
-                    .padding(top = 2.dp, start = 10.dp, end = 10.dp, bottom = 2.dp)
+                    .padding(top = 2.dp, start = 10.dp, end = 10.dp, bottom = 10.dp)
                     .fillMaxWidth(),
             ) {
-                Column {
-                    Text(name, fontWeight = FontWeight.Bold, lineHeight = 10.sp)
+                Column(
+                    modifier = Modifier.weight(1f).height(120.dp),
+                    verticalArrangement = Arrangement.SpaceBetween
+                ) {
+                    // album title
+                    Text(name, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                    // play / shuffle
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_play_arrow_24),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(35.dp)
+                                .clickable(true, onClick = {
+                                    viewModel.musicRepository.replaceWaitingList(musics)
+                                }),
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
+                        Icon(
+                            painter = painterResource(if (shuffleOn) { R.drawable.outline_shuffle_on_24}
+                            else {
+                                R.drawable.outline_shuffle_24
+                            }),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(35.dp)
+                                .clickable(enabled = false, onClick = { /*shuffleOn = !shuffleOn*/ }),
+                            tint = if (shuffleOn) MaterialTheme.colorScheme.primary.copy(alpha = 0.38f) else MaterialTheme.colorScheme.onBackground.copy(alpha = 0.38f)
+                        )
+                    }
                 }
                 Box(
                     modifier = Modifier
-                        .size(60.dp)
+                        .size(120.dp)
                         .border(1.dp, MaterialTheme.colorScheme.onBackground, RoundedCornerShape(16.dp))
                         .clip(RoundedCornerShape(16.dp))
                 ) {
@@ -95,36 +126,6 @@ fun AlbumUi(albumId: Int) {
                             .fillMaxSize(),
                         model = image,
                         contentDescription = null
-                    )
-                }
-            }
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .padding(start = 10.dp, end = 10.dp, bottom = 5.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement
-                        .spacedBy(8.dp),
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.outline_play_arrow_24),
-                        contentDescription = "",
-                        modifier = Modifier.clickable(true, onClick = {
-                            viewModel.musicRepository.replaceWaitingList(musics)
-                        }),
-                        tint = MaterialTheme.colorScheme.onBackground
-                    )
-                    Icon(
-                        painter = painterResource(if (shuffleOn) { R.drawable.outline_shuffle_on_24}
-                        else {
-                            R.drawable.outline_shuffle_24
-                        }),
-                        contentDescription = "",
-                        modifier = Modifier.clickable(enabled = false, onClick = { /*shuffleOn = !shuffleOn*/ }),
-                        tint = if (shuffleOn) MaterialTheme.colorScheme.primary.copy(alpha = 0.38f) else MaterialTheme.colorScheme.onBackground.copy(alpha = 0.38f)
                     )
                 }
             }
